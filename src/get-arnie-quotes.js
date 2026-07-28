77cf51e9b5041259d88860dc6a2d84b8f5ffff78
Ajinkya Buva <ajinkyabuva7@gmail.com>
2026-07-28 10:59:42 +1000

    function get-arnie-quotes implemented

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -5,7 +5,14 @@ const { httpGet } = require('./mock-http-interface');
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  return Promise.all(urls.map(async (url) => {
+    const response = await httpGet(url);
+    const { message } = JSON.parse(response.body);
+
+    return response.status === 200
+      ? { 'Arnie Quote': message }
+      : { 'FAILURE': message };
+  }));
 };
 
 module.exports = {
